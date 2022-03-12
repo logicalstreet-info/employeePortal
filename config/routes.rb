4b042e4a4b748571d68b267e9130885a521e02f8
dharalogical <dhara.logicalstreet@gmail.com>
2022-03-12 14:54:31 +0530

    add Organization CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
+  get 'organizations/index'
+  resources :organizations
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
