0537497f6830103db5d177c4129868a887f24809
dharalogical <dhara.logicalstreet@gmail.com>
2022-05-13 17:21:39 +0530

    list of employee's upcoming one month birthdays

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @users = User.where(
+      :organization_id => current_user.organization_id)
+    @bday = @users.where("(DATE_PART('month', birth_date) = ? AND DATE_PART('day', birth_date) >= ?) OR (DATE_PART('month', birth_date) = ? AND DATE_PART('day', birth_date) <= ?)", Date.current.month, Date.current.day, Date.current.month + 1 ,
+    Date.current.day)
   end
 end
