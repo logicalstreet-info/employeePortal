3c8ecc9d47096743a261988bf8d2158d7e079772
dharalogical <dhara.logicalstreet@gmail.com>
2022-05-13 15:34:19 +0530

    add global helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def date_format(date)
+    date.strftime("%d-%B-%Y")
+  end
+  
+  def time_format(date)
+    date.strftime("%I:%M %p")
+  end
 end
--- app/helpers/leave_applications_helper.rb
+++ app/helpers/leave_applications_helper.rb
@@ -1,10 +1,2 @@
 module LeaveApplicationsHelper
-  
-  def date_format(date)
-    date.strftime("%d-%B-%Y")
-  end
-
-  def time_format(date)
-    date.strftime("%I:%M %p")
-  end
 end
