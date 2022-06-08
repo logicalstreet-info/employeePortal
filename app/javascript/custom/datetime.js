679e75dce62e33fde174714695000e520b78bdb7
dharalogical <dhara.logicalstreet@gmail.com>
2022-06-08 10:54:20 +0530

    Changed date formate in flatpicker

--- app/javascript/custom/datetime.js
+++ app/javascript/custom/datetime.js
@@ -1,2 +1,2 @@
 flatpickr(".timepicker-input", {enableTime: true, noCalendar: true, dateFormat: "H:i"});
-flatpickr(".datepicker-input", {enableTime: false, dateFormat: "Y-m-d",});
+flatpickr(".datepicker-input", {enableTime: false, dateFormat: "d-m-Y",});
