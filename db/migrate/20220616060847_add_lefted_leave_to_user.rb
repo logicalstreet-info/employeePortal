d0c52491823d391cac4f541dcfb0618b569fc8e7
dharalogical <dhara.logicalstreet@gmail.com>
2022-06-20 09:49:57 +0530

    Changed in migration for remove column

--- db/migrate/20220616060847_add_lefted_leave_to_user.rb
+++ db/migrate/20220616060847_add_lefted_leave_to_user.rb
@@ -1,6 +1,5 @@
 class AddLeftedLeaveToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :lefted_leave, :float, default: 0.0
-    add_column :organizations, :given_leave_to_employee, :float
   end
 end
