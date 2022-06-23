0d9c414ca2bf99058340f392663fe45ee88735ca
dharalogical <dhara.logicalstreet@gmail.com>
2022-06-23 11:03:55 +0530

    Resolve reload issue on flatepicker

--- app/javascript/custom/datetime.js
+++ app/javascript/custom/datetime.js
@@ -1,2 +1,4 @@
-flatpickr(".timepicker-input", {enableTime: true, noCalendar: true, dateFormat: "H:i"});
-flatpickr(".datepicker-input", {enableTime: false, dateFormat: "d-m-Y",});
+$(document).on('turbo:load', function() {
+  flatpickr(".timepicker-input", {enableTime: true, noCalendar: true, dateFormat: "H:i"});
+  flatpickr(".datepicker-input", {enableTime: false, dateFormat: "d-m-Y",});
+});
