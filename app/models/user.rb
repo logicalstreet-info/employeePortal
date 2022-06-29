4e626cc5f7127ebfdbe5179a6859ff7bd5ba5c9e
kajal259 <kajal.logicalstreet@gmail.com>
2022-06-29 17:08:54 +0530

    changed validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email, presence: true
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates_format_of :mobile_number, :parent_mobile_number,
     :with => /[0-9]{9}/,
