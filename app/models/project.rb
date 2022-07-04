0963174a618e3c5a97339ee8cb168b20ee0368e1
dharalogical <dhara.logicalstreet@gmail.com>
2022-07-04 09:48:15 +0530

    Listing all employees and spended hours into projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,10 @@ class Project < ApplicationRecord
   has_many :users, through: :periods
   has_many :updates, dependent: :destroy
   belongs_to :organization
+
   validates :name, presence: true
+
+  def self.count_hour(project, user)
+    @total_hours = Update.where(project_id: project, user_id: user).sum(:total_hours)
+  end
 end
