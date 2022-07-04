bf5eafd582bc459a22fb2a9cdc3db8b3940ce118
dharalogical <dhara.logicalstreet@gmail.com>
2022-07-04 17:31:01 +0530

    Added filter on projects show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,11 @@ class Project < ApplicationRecord
 
   validates :name, presence: true
 
-  def self.count_hour(project, user)
-    @total_hours = Update.where(project_id: project, user_id: user).sum(:total_hours)
+  def self.count_hour(project, user, start_date, end_date)
+    if start_date.present? && end_date.present?
+      @total_hours = Update.where("project_id = ? AND user_id = ? AND DATE(created_at) BETWEEN ? AND ?", project, user, start_date.to_date.strftime('%Y-%m-%d'), end_date.to_date.strftime('%Y-%m-%d')).sum(:total_hours)
+    else
+      @total_hours = Update.where("project_id = ? AND user_id = ? AND created_at BETWEEN ? AND ?", project, user, Date.today.beginning_of_month, Date.today.end_of_month).sum(:total_hours)
+    end
   end
 end
