2e2896e705f75a19222d3b1d07eb6263fd8190f0
dharalogical <dhara.logicalstreet@gmail.com>
2022-07-05 17:43:01 +0530

    Remove configuration for flatepickr

--- app/javascript/custom/datetime.js
+++ app/javascript/custom/datetime.js
@@ -1,6 +1,6 @@
 $(document).on('turbo:load', function() {
   flatpickr(".timepicker-input", { allowInput: true, altInput: true,
      enableTime: true, noCalendar: true, dateFormat: "H:i:K"});
-  flatpickr(".datepicker-input", { allowInput: true, altInput: true,
+  flatpickr(".datepicker-input", { allowInput: true,
      enableTime: false, dateFormat: "d-m-Y"});
 });
